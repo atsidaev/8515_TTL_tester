0be56b8950e8a617cb8b80f4c5ccc79ad70ffe14
Alexander Tsidaev <a.tsidaev@gmail.com>
2016-04-21 14:04:51 +0500

    Magic consts decomposition to bitwise operators (tested)

--- test_019.c
+++ test_019.c
@@ -6,29 +6,29 @@ void test_019(void)
         sym[3]=12;
         res=0;
         sym[0]=12;
-        Direct_L=195;
-        Direct_H=135;
-        Port_L=62;
-        Port_H=125;
-        Port_L=60;
-        Port_H=121; 
+        Direct_L=(BIT_L0 | BIT_L1 | BIT_L6 | BIT_L7);
+        Direct_H=(BIT_H7 | BIT_H2 | BIT_H1 | BIT_H0);
+        Port_L=(BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+        Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H2 | BIT_H0);
+        Port_L=(BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+        Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H0); 
         delay_ms(1);       
-        if ((Pin_L==0) && (Pin_H==1)) res=1;
+        if ((Pin_L==0) && (Pin_H==(BIT_H0))) res=1;
             else res=0;            
-        Port_L=61;
-        Port_L=60;
-        Port_H=123;
-        Port_H=121;
+        Port_L=(BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+        Port_L=(BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+        Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H1 | BIT_H0);
+        Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H0);
         delay_ms(1);
-        if ((Pin_L==4) && (Pin_H==9) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L2)) && (Pin_H==(BIT_H3 | BIT_H0)) && (res==1)) res=1;
             else res=0;
         for (n=0;n<14;n++) {    
-            Port_L=61;
-            Port_L=60;
-            Port_H=123;
-            Port_H=121;}
+            Port_L=(BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+            Port_L=(BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5);
+            Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H1 | BIT_H0);
+            Port_H=(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H0);}
         delay_ms(1);
-        if ((Pin_L==60) && (Pin_H==121) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5)) && (Pin_H==(BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H0)) && (res==1)) res=1;
             else res=0;       
         if (res==1) sym[0]=10;
             else sym[0]=11;
--- test_74161.c
+++ test_74161.c
@@ -6,54 +6,54 @@ void test_74161(void)
         sym[3]=1;
         res=0;
         sym[0]=12;
-        Direct_L=255;
-        Direct_H=193;
-        Port_H=129;
+        Direct_L=(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L5 | BIT_L6 | BIT_L7);
+        Direct_H=(BIT_H7 | BIT_H6 | BIT_H0);
+        Port_H=(BIT_H7 | BIT_H0);
         Port_L=0;   //Clear
         delay_ms(1);
-        Port_L=2;   //CLK
+        Port_L=(BIT_L1);   //CLK
         delay_ms(1);                
-        if ((Pin_L==2) && (Pin_H==129)) res=1;
+        if ((Pin_L==(BIT_L1)) && (Pin_H==(BIT_H7 | BIT_H0))) res=1;
             else res=0;
 
-        Port_L=1;   //CLK   
+        Port_L=(BIT_L0);   //CLK   
         delay_ms(1);
-        Port_L = 29; //D=0,A=B=C=1, CLK = 0        
-        Port_H=1;   //Load
+        Port_L = (BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4); //D=0,A=B=C=1, CLK = 0        
+        Port_H=(BIT_H0);   //Load
         delay_ms(1);
-        Port_L = 31; //D=0,A=B=C=1, CLK = 1
+        Port_L = (BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4); //D=0,A=B=C=1, CLK = 1
         delay_ms(1);
-        Port_H=193;  //ENT=1
-        Port_L=95;   //ENP=1
+        Port_H=(BIT_H7 | BIT_H6 | BIT_H0);  //ENT=1
+        Port_L=(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);   //ENP=1
         delay_ms(1);        
-        Port_L=93;  //CLK
+        Port_L=(BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);  //CLK
         delay_ms(1);
-        if ((Pin_L==93) && (Pin_H==221) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6)) && (Pin_H==(BIT_H7 | BIT_H6 | BIT_H4 | BIT_H3 | BIT_H2 | BIT_H0)) && (res==1)) res=1;
             else res=0;
-        Port_L = 95;
+        Port_L = (BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);
         delay_ms(1);
-        if ((Pin_L==95) && (Pin_H==225) && (res==1)) res=1;        
+        if ((Pin_L==(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6)) && (Pin_H==(BIT_H7 | BIT_H6 | BIT_H5 | BIT_H0)) && (res==1)) res=1;        
             else res=0;
-        Port_L = 93;    //CLK
+        Port_L = (BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);    //CLK
         delay_ms(1);
-        Port_L = 95;    //CLK
+        Port_L = (BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);    //CLK
         delay_ms(1);
-        if ((Pin_L==95) && (Pin_H==229) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6)) && (Pin_H==(BIT_H7 | BIT_H6 | BIT_H5 | BIT_H2 | BIT_H0)) && (res==1)) res=1;
            else res=0;
            
-        Port_L = 29;    //CLK
+        Port_L = (BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4);    //CLK
         delay_ms(1);
-        Port_L = 31;    //CLK
+        Port_L = (BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4);    //CLK
         delay_ms(1);
-        if ((Pin_L==31) && (Pin_H==229) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4)) && (Pin_H==(BIT_H7 | BIT_H6 | BIT_H5 | BIT_H2 | BIT_H0)) && (res==1)) res=1;
             else res=0;
         for (i= 0;i<6;i++) {
-            Port_L = 93;    //CLK
+            Port_L = (BIT_L0 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);    //CLK
             delay_ms(1);
-            Port_L = 95;    //CLK
+            Port_L = (BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6);    //CLK
             delay_ms(1);
         }
-        if ((Pin_L==95) && (Pin_H==255) && (res==1)) res=1;
+        if ((Pin_L==(BIT_L0 | BIT_L1 | BIT_L2 | BIT_L3 | BIT_L4 | BIT_L6)) && (Pin_H==(BIT_H7 | BIT_H6 | BIT_H5 | BIT_H4 | BIT_H3 | BIT_H2 | BIT_H1 | BIT_H0)) && (res==1)) res=1;
             else res=0;      
         if (res==1) sym[0]=10;
             else sym[0]=11;
